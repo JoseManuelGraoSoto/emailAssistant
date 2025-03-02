b65b301b148a34b893a5bebde96560f7765533b9
jmgs24 <josegrao19@gmail.com>
2025-03-02 15:10:20 +0100

    First commit

---
+++ src/main/java/com/email/emailSP/EmailSpApplication.java
@@ -0,0 +1,13 @@
+package com.email.emailSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/email/emailSP/EmailSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.email.emailSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
